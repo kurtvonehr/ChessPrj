161ffdfef4f648dccd901cba042ce46ff79f335f
4fab <kvonehr@gmail.com>
2014-03-03 09:25:46 -0500

    Added Piece Classes

---
+++ ChessPiece.java
@@ -0,0 +1,23 @@
+package chess;
+
+public abstract class ChessPiece implements IChessPiece {
+
+	@Override
+	public Player player() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String type() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isValidMove(Move move, IChessPiece[][] board) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
