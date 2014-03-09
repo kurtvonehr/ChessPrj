4e953fbf9fbff8f54848b528ea03aca5f5e0ae5f
JacobMcKim <mckim.jacob@gmail.com>
2014-03-09 01:05:10 -0500

    Update IChessPiece.java
    Added the inGrid definition and cleaned up class a bit.

--- IChessPiece.java
+++ IChessPiece.java
@@ -11,36 +11,56 @@ package chess;
 public interface IChessPiece {
 
 
-   /**
-    * Return the player that owns this piece.
-    *
-    * @return the player that owns this piece.
-    */
+   /****************************************************************
+   * This method returns the player that owns this piece.
+   *
+   * @return the player that owns this piece.
+   * 
+   *****************************************************************/
    Player player();
 
-   /**
-    * Return the type of this piece ("King", "Queen", "Rook", etc.).  Note:  In this case "type" refers to the game
-    * of chess, not the type of the Java class.
-    *
-    * @return the type of this piece
-    */
+
+   /****************************************************************
+   * This method returns the type of this piece ("King", "Queen", 
+   * "Rook", etc.).  Note:  In this case "type" refers to the game.
+   * of chess, not the type of the Java class.
+   *
+   * @return the type of this piece
+   * 
+   *****************************************************************/
    String type();
 
-   /**
-    * Returns whether the piece at location {@code [move.fromRow, move.fromColumn]} is allowed to move to location
-    * {@code [move.fromRow, move.fromColumn]}.
-    *
-    * Note:  Pieces don't store their own location (because doing so would be redundant).  Therefore,
-    * the {@code [move.fromRow, move.fromColumn]} component of {@code move} is necessary.
-    * {@code this} object must be the piece at location {@code [move.fromRow, move.fromColumn]}.
-    * (This method makes no sense otherwise.)
-    *
-    * @param move  a {@link W13project3.Move} object describing the move to be made.
-    * @param board the {@link W13project3.IChessBoard} in which this piece resides.
-    * @return {@code true} if the proposed move is valid, {@code false} otherwise.
-    * @throws IndexOutOfBoundsException if either {@code [move.fromRow, move.fromColumn]} or {@code [move.toRow,
-    *                                   move.toColumn]} don't represent valid locations on the board.
-    * @throws IllegalArgumentException  if {@code this} object isn't the piece at location {@code [move.fromRow, move.fromColumn]}.
-    */
+
+   /****************************************************************
+   * This method returns whether the piece at location 
+   * {@code [move.fromRow, move.fromColumn]} is allowed to move to 
+   * location {@code [move.fromRow, move.fromColumn]}.
+   *
+   * @param move  a {@link ChessPrj.Move} object describing the 
+   * move to be made.
+   * 
+   * @param board the {@link ChessPrj.IChessBoard} in which this
+   * piece resides.
+   * 
+   * @return {@code true} if the proposed move is valid, 
+   * {@code false} otherwise.
+   * 
+   * @throws IndexOutOfBoundsException if either {@code [move.fromRow,
+   * move.fromColumn]} or {@code [move.toRow, move.toColumn]} don't 
+   * represent valid locations on the board.
+   * 
+   * @throws IllegalArgumentException  if {@code this} object isn't 
+   * the piece at location {@code [move.fromRow, move.fromColumn]}.
+   * 
+   *****************************************************************/
    boolean isValidMove(Move move, IChessPiece[][] board);
+   
+   /****************************************************************
+   * This method validates that a move postion is within the grid.
+   *
+   * @return Whether or not the move is in the grid. 
+   * 
+   *****************************************************************/
+   boolean inGrid (int xPos, int yPos);
+
 }
