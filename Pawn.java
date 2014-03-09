067d3858844c11b4f4d70ec2fb1c4b2ee17ceecd
JacobMcKim <mckim.jacob@gmail.com>
2014-03-09 01:36:26 -0500

    Update Pawn.java
    Built in the super call for the isValidMove and added in a space to fill out pawn specific move logic. I also implemented the type () method.

--- Pawn.java
+++ Pawn.java
@@ -1,5 +1,64 @@
 package chess;
 
 public class Pawn extends ChessPiece {
+  
+	//---------------------------------------------------------------//	
+	// Class Variable Definitions                                    //
+	//---------------------------------------------------------------//
+	
+	private ChessModel model;
+	
+	//---------------------------------------------------------------//	
+	// Class Constructors                                            //
+	//---------------------------------------------------------------//	
+	
+	/*****************************************************************
+  	The primary constructor for the bishop peice. It creates a bishop
+  	for the given player specified.
+        
+	 @return None 
+    
+  	*****************************************************************/
+  	public Pawn () {
+		
+		// Get the model object for help with control.
+		model = ChessModel.getInstance ();
+		
+	}
 
+	//--------------------------------------------------------------//	
+	// Override Function Definitions					      		//
+	//--------------------------------------------------------------//  
+
+	/*****************************************************************
+  	An overriden acessor method that returns the type of chess peice
+  	this class peice is.
+        
+  	@return The class type of pawn as a string. 
+    
+  	*****************************************************************/
+  	@Override
+  	public String type () {
+    
+    	return "Pawn";
+    
+  	}
+  
+	@Override
+	public boolean isValidMove(Move move, IChessPiece[ ][ ] board) {
+		
+		// Preform the generic background check.
+		super(move,board);
+		
+		// continue with checking for peice specifics.
+		if (result)
+		{
+			//TODO implement pawn search.
+		}
+		
+		// Return the result of the operation.
+		return result;
+		
+	}
+	
 }
