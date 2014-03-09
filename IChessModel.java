ed780f1dc8f9c01f4b870a76dd0ca2fa25d9b0a7
JacobMcKim <mckim.jacob@gmail.com>
2014-03-09 00:56:28 -0500

    Fixed to be an interface
    Fixed this to be an interface.

--- IChessModel.java
+++ IChessModel.java
@@ -9,44 +9,108 @@ package chess;
 
 public interface IChessModel {
 
-	private IChessPiece[][] board; 
-	 private Player player; 
-	 // declare other instance variables as needed 
-	 
-	 public ChessModel() { 
-	 // complete this 
-	 } 
-	 
-	 public boolean isComplete() { 
-	 return false; 
-	 } 
-	 
-	 public boolean isValidMove(Move moce) { 
-	 // complete this 
-	 } 
-	 
-	 public void move(Move move) { 
-	 // complete this 
-	 } 
-	 
-	 public boolean inCheck(Player p) { 
-	 return false; 
-	 } 
-	 
-	 public Player currentPlayer() { 
-	 // complete this 
-	 } 
-	 
-	 public int numRows() { 
-	 // complete this 
-	 } 
-	 
-	 public int numColumns() { 
-	 // complete this 
-	 } 
-	 
-	 public IChessPiece pieceAt(int row, int column) { 
-	 // complete this 
-	 } 
+
+     /****************************************************************
+     * This method returns whether the game is complete.
+     *
+     * @return {@code true} if complete, {@code false} otherwise.
+     * 
+     *****************************************************************/
+     public boolean isComplete();
+     
+     
+     /****************************************************************
+     * This method returns whether the piece at location {
+     * @code [move.fromRow, move.fromColumn]} is allowed to move to 
+     * location{@code [move.fromRow, move.fromColumn]}.
+     *
+     * @param move a {@link Move} object describing the move to be made.
+     * 
+     * @return {@code true} if the proposed move is valid, {@code false} 
+     * otherwise.
+     * 
+     * @throws IndexOutOfBoundsException if either {@code [move.fromRow, 
+     * move.fromColumn]} or {@code [move.toRow,move.toColumn]} don't 
+     * represent valid locations on the board.
+     * 
+     *****************************************************************/
+     public boolean isValidMove(Move move);
+     
+     
+     /****************************************************************
+     * This method moves the piece from location {@code [move.fromRow,
+     * move.fromColumn]} to location {@code [move.fromRow,move.
+     * fromColumn]}.
+     *
+     * @param move a {@link Move} object describing the move to be
+     * made.
+     * 
+     * @returns None
+     * 
+     * @throws IndexOutOfBoundsException if either {@code [move.fromRow,
+     * move.fromColumn]} or {@code [move.toRow,move.toColumn]} don't 
+     * represent valid locations on the board.
+     * 
+     *****************************************************************/
+     public void move(Move move);
+     
+     
+     /****************************************************************
+     * This method reports whether the current player is in check.
+     *
+     * @return {@code true} if the current player is in check, 
+     * {@code false} otherwise.
+     * 
+     *****************************************************************/
+     public boolean inCheck(Player p);
+     
+     
+     /****************************************************************
+     * This method returns the current player.
+     *
+     * @return the current player.
+     * 
+     *****************************************************************/
+     public Player currentPlayer(); 
+     
+     
+     /****************************************************************
+     * This method reports back the number of rows that are on the 
+     * board.
+     *
+     * @return The row count as an integer.
+     * 
+     *****************************************************************/
+     public int numRows();
+     
+     
+     /****************************************************************
+     * This method reports back the number of columns that are on the 
+     * board.
+     *
+     * @return The column count as an integer.
+     * 
+     *****************************************************************/
+     public int numColumns();
+     
+     
+     /****************************************************************
+     * This method returns the {@code ChessPiece} object at location 
+     * {@code [row, column]}.
+     *
+     * @param row    the row (numbered {@code 0} through 
+     * {@code numRows -1}
+     * 
+     * @param column the row (numbered {@code 0} through 
+     * {@code numColumns -1}
+     * 
+     * @return the {@code ChessPiece} object at location 
+     * {@code [row, column]}.
+     * 
+     * @throws IndexOutOfBoundsException if {@code [row, column]} is 
+     * not a valid location on the board.
+     * 
+     *****************************************************************/
+     public IChessPiece pieceAt(int row, int column);
 
 }
