eb2491484065de2f8cbf5b2e7adc4c145c7c5e54
4fab <kvonehr@gmail.com>
2014-03-09 21:05:35 -0400

    Added board init

--- ChessModel.java
+++ ChessModel.java
@@ -28,7 +28,7 @@ public class ChessModel implements IChessModel {
   	private ChessModel instance;
   
 	/* The game board holding all game pieces. Board is 8x8 */
-	private IChessPiece[][] board = new ChessPiece[8][8]; 
+	private IChessPiece[][] board;
 	
 	/* The first player opponent. */
 	private Player White = Player.WHITE;
@@ -39,13 +39,45 @@ public class ChessModel implements IChessModel {
 	//---------------------------------------------------------------//	
 	// Class Constructors                                            //
 	//---------------------------------------------------------------//	
-
+	
 	private ChessModel() { 
 	
+		//Init blank board
+		for(int i = 0; i<8; i++)
+			for(int j = 0; j<8; j++){
+				board[i][j] = null;
+			}
+		//Place Black pieces onto board
+		board[7][0] = new Rook(Player.BLACK);
+		board[7][1] = new Knight(Player.BLACK);
+		board[7][2] = new Bishop(Player.BLACK);
+		board[7][3] = new Queen(Player.BLACK);
+		board[7][4] = new King(Player.BLACK);
+		board[7][5] = new Bishop(Player.BLACK);
+		board[7][6] = new Knight(Player.BLACK);
+		board[7][7] = new Rook(Player.BLACK);
 		
+		for(int i =0; i<8;i++){
+			board[6][i] = new Pawn(Player.BLACK);
+		}
 		
+		//Place White pieces onto board
+		board[0][0] = new Rook(Player.WHITE);
+		board[0][1] = new Knight(Player.WHITE);
+		board[0][2] = new Bishop(Player.WHITE);
+		board{0][3] = new Queen(Player.WHITE);
+		board[0][4] = new King(Player.WHITE);
+		board[0][5] = new Bishop(Player.WHITE);
+		board[0][6] = new Knight(Player.WHITE);
+		board[0][7] = new Rook(Player.WHITE);
 		
-	}
+		for(int i =0; i<8;i++){
+			board[6][i] = new Pawn(Player.WHITE);
+		}
+		
+	
+		}
+	
 	
 	//--------------------------------------------------------------//	
 	// Function Definitions					      					//
