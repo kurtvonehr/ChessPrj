e46f5d1d09d47e60a8a51e8fbf8ff0a6657bf118
JacobMcKim <mckim.jacob@gmail.com>
2014-03-15 15:19:30 -0400

    Create ChessPanel.java

---
+++ ChessPanel.java
@@ -0,0 +1,195 @@
+package chess;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * Responsible for presenting the GUI, the user's actions, and updating the view
+ * Implement a standard form of chess (White moves first, then black)
+ * Only allow valid moves (i.e. isMoveValid())
+ * changed
+ */
+
+@SuppressWarnings("serial")
+public class ChessPanel extends JPanel {
+
+	private ChessModel model;
+	
+	private JButton[][] boardButtons;
+	private Image[][] boardImages;
+	
+	private JPanel top_panel, bottom_panel, grid;
+	private JLabel message;
+	private JButton moveButton;
+	
+	private ButtonListener bl = new ButtonListener();
+
+	private int rows, cols;
+	public int count;
+	public Move moving;
+	
+	// These are purely for visual purposes...
+	public static final int QUEEN=0, KING=1, ROOK=2, KNIGHT=3, BISHOP=4, PAWN=5;
+	public static final int BLK=0, WHT=1;
+	public static final int[] MAIN_ROW = {ROOK, KNIGHT, BISHOP, KING, QUEEN, BISHOP, KNIGHT, ROOK};
+	
+	public ChessPanel() {
+		
+		model = new ChessModel();
+		// Gets number of Rows and Columns from ChessModel
+		rows = model.numRows();
+		cols = model.numColumns();
+		count = 0;
+		
+		// Inst. array of chess board 'cells' and the array of icons used for the pieces
+		boardButtons = new JButton[rows][cols];
+		boardImages = new Image[2][6];
+		
+		// Inst. bottom panel and adds "Move" button to it
+		bottom_panel = new JPanel();
+		moveButton = new JButton("Move");
+		moveButton.addActionListener((ActionListener) bl);
+		bottom_panel.add(moveButton);
+		
+		// Inst. top panel and adds message label where messages can be displayed
+		top_panel = new JPanel();
+		message = new JLabel("This is a game of chess...that doesn't work");
+		top_panel.add(message);
+		
+		// Builds out all of the buttons, which are the squares of the chess board
+		grid = new JPanel(new GridLayout (rows, cols));
+		for (int row = 0; row < 8; row++){
+			for (int col = 0; col < 8; col++){
+				boardButtons[row][col] = new JButton("");
+				boardButtons[row][col].addActionListener(new ButtonListener());
+				grid.add(boardButtons[row][col]);
+				
+				/** Sets blank background image to each piece */
+				boardButtons[row][col].setIcon(new ImageIcon(new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB)));
+				
+				/** Sets background colors of buttons to black or white, as required by rules of chess **/
+				if ( (row % 2 == 0 && col % 2 == 0) || (row % 2 == 1 && col % 2 ==1)) {
+					boardButtons[row][col].setBackground(Color.white);
+				}
+				else {
+					boardButtons[row][col].setBackground(Color.black);
+				}
+			}
+		}
+		
+		/** Builds boardImages[][] array and then displays new game board pieces */
+		createImages();
+		displayBoard();
+		
+		add(top_panel);
+		add(grid);
+		add(bottom_panel);
+		
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+	}
+
+	// updates the board and redraws the icons ?
+	private void displayBoard() {
+		for (int ii = 0; ii < rows; ii++) {
+			// TODO This is a static build of the icons, just to see them work properly.
+			// TODO The buttons should be aware of their type and set accordingly ??
+			boardButtons[0][ii].setIcon(new ImageIcon(boardImages[BLK][MAIN_ROW[ii]]));
+			boardButtons[1][ii].setIcon(new ImageIcon(boardImages[BLK][PAWN]));
+			boardButtons[rows-1][ii].setIcon(new ImageIcon(boardImages[WHT][MAIN_ROW[ii]]));
+			boardButtons[rows-2][ii].setIcon(new ImageIcon(boardImages[WHT][PAWN]));
+		}
+		for (int ii = 0; ii < rows; ii++) {
+			for (int jj = 0; jj < cols; jj++) {
+				// Remove piece, if captured
+				if (model.isCaptured())
+					boardButtons[ii][jj].setIcon(new ImageIcon(new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB)));
+			}
+		}
+	}
+	
+	public static int getClickOne() {
+		int row;
+		int column;
+		return x, y;
+	}
+	public static int getClickTwo() {
+		re
+	}
+	
+	/** Place other helper methods below */
+	
+	private void createImages() {
+		// Loads and creates image for each piece from the "sprite"
+		// Sprite's original location: http://i.stack.imgur.com/memIO.png
+		try {
+			BufferedImage bi = ImageIO.read(new File("src/chess/pieces.png"));
+			for (int i=0; i < 2; i++) {
+				for (int j=0; j < 6; j++) {
+					boardImages[i][j] = bi.getSubimage(j * 64, i * 64, 64, 64);
+				}
+			}
+		}
+		catch (Exception e){			// ??
+			e.printStackTrace();		// ??
+			System.exit(1);				// close window on error??
+		}
+	}
+	
+	/** Inner Class represents action listener for buttons **/
+	private class ButtonListener implements ActionListener {
+		
+		//private int fromRow, fromColumn, toRow, toColumn;
+		public void actionPerformed(ActionEvent e) {
+			// loop through all the [rows,cols] and check for action event
+			moving = new Move();
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < cols; j++) {
+					if (e.getSource() == boardButtons[i][j]) {
+						
+						if (count == 0) {
+							// set coord. variables to Move Object move
+							moving.fromRow = i;
+							moving.fromColumn = j;
+							// instantiate Piece object?? with the pieceAt; use below
+							ChessPiece piece =;
+							count = count++;
+						}
+						if (count == 1) {
+							// sets Move Object 2nd coords.
+							moving.toRow = i;
+							moving.toColumn = j;
+							// check if move is valid
+							if (isValidMove(moving, piece)) {
+								model.move(moving.fromRow, moving.fromColumn, moving.toRow, moving.toColumn);
+								count = 0;
+							}
+						}
+						// Shows coordinates in message pane (testing)
+						//message.setText("( " + i + "," + j + ")");	
+					}
+				}
+			}
+			
+			
+			
+			/* **************************************
+			 * MOVE Button:
+			 * *************************************/
+//			if (e.getSource() == moveButton) {
+//				model.currentPlayer().move(play);
+//			}
+		}
+	}
+}
