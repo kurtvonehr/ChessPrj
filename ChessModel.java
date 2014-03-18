16793470a35ecea09ce7c08c72e8ac4e53af3e4e
4fab <kvonehr@gmail.com>
2014-03-17 21:04:52 -0400

    swapped color sides, it was backwards

--- ChessModel.java
+++ ChessModel.java
@@ -51,32 +51,32 @@ public class ChessModel implements IChessModel {
 		board = new ChessPiece[this.numRows()][this.numColumns()];
 		
 		//Place White pieces onto board
-		board[0][0] = new Rook(Player.WHITE);
-		board[0][1] = new Knight(Player.WHITE);
-		board[0][2] = new Bishop(Player.WHITE);
-		board[0][3] = new Queen(Player.WHITE);
-		board[0][4] = new King(Player.WHITE);
-		board[0][5] = new Bishop(Player.WHITE);
-		board[0][6] = new Knight(Player.WHITE);
-		board[0][7] = new Rook(Player.WHITE);
+		board[0][0] = new Rook(Player.BLACK);
+		board[0][1] = new Knight(Player.BLACK);
+		board[0][2] = new Bishop(Player.BLACK);
+		board[0][3] = new Queen(Player.BLACK);
+		board[0][4] = new King(Player.BLACK);
+		board[0][5] = new Bishop(Player.BLACK);
+		board[0][6] = new Knight(Player.BLACK);
+		board[0][7] = new Rook(Player.BLACK);
 		
 		//Place Black pieces onto board
-		board[7][0] = new Rook(Player.BLACK);
-		board[7][1] = new Knight(Player.BLACK);
-		board[7][2] = new Bishop(Player.BLACK);
-		board[7][3] = new Queen(Player.BLACK);
-		board[7][4] = new King(Player.BLACK);
-		board[7][5] = new Bishop(Player.BLACK);
-		board[7][6] = new Knight(Player.BLACK);
-		board[7][7] = new Rook(Player.BLACK);
+		board[7][0] = new Rook(Player.WHITE);
+		board[7][1] = new Knight(Player.WHITE);
+		board[7][2] = new Bishop(Player.WHITE);
+		board[7][3] = new Queen(Player.WHITE);
+		board[7][4] = new King(Player.WHITE);
+		board[7][5] = new Bishop(Player.WHITE);
+		board[7][6] = new Knight(Player.WHITE);
+		board[7][7] = new Rook(Player.WHITE);
 		
 		// Place black pawns.
 		for(int i = 0; i < boardDim; i++)
-			board[6][i] = new Pawn(Player.BLACK);
+			board[6][i] = new Pawn(Player.WHITE);
 		
 		// Place white pawns.
 		for(int i = 0; i< boardDim; i++)
-			board[1][i] = new Pawn(Player.WHITE);
+			board[1][i] = new Pawn(Player.BLACK);
 		
 		// Define the first current player.
 		currentPlayer = White;
