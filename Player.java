2c5d65b962c5c6c252c082fde5617842bc62ee2f
JacobMcKim <mckim.jacob@gmail.com>
2014-03-20 01:11:54 -0400

    Finalized

--- Player.java
+++ Player.java
@@ -1,20 +1,28 @@
 package chess;
 
-/**
- * A player of a chess game.
- *
- * @author Kurt VonEhr, Jacob Mckim, 
- */
+/*--------------------------------------------------------------------*
+* Player.java                             		             		  *
+*---------------------------------------------------------------------*
+* Description - An enumeration to name off the players currently 	  *
+* Involved in the game. 					 					  	  *
+*---------------------------------------------------------------------*
+* Project: Project 3 : Chess 	                                      *
+* Author : McKim A. Jacob, Vonehr Kurt, Aernouts Kenneth	          *
+* Date Of Creation: 3 - 1 - 2014                                      *                          
+*---------------------------------------------------------------------*/
+
 public enum Player {
 	
    BLACK, WHITE, NOBODY;
 
-   /**
+   /****************************************************************
     * Return the {@code Player} whose turn is next.
     *
-    * @return the {@code Player} whose turn is next
-    */
+    * @return the {@code Player} whose turn is next.
+    * 
+    ***************************************************************/
    public Player next() {
       return this == BLACK ? WHITE : BLACK;
    }
+   
 }
